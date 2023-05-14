e4997007235b71ccafafe4d296baf435f9bb4b8b
Muhammad SA <MuhammadSA211999>
2023-05-14 23:36:56 +0600

    utility-types record-type index-sig type

--- dist/utility-type.js
+++ dist/utility-type.js
@@ -1,1 +1,13 @@
 "use strict";
+const myObj2 = {
+    a: '1',
+    b: '2',
+    c: '3'
+    //more string property will continueing
+};
+const myObj3 = {
+    a: '1',
+    b: '2',
+    c: '3'
+    //property type MyObj3 er a,b,c porzonto hobe
+};
--- src/utility-type.ts
+++ src/utility-type.ts
@@ -12,3 +12,32 @@ type Address = Omit<Person, 'name' | 'contact'>
 type Optional = Partial<Person>
 //Partial hocce optional
 type RequiredProps = Required<Person>
+
+//Record type with Literel and Index-signature 
+//literel
+type MyObj1 = {
+    a: string;
+    b: string;
+    c: string;
+}
+//index-signature
+type MyObj2 = {
+    [key: string]: string
+    //  key type: value return type
+}
+const myObj2: MyObj2 = {
+    a: '1',
+    b: '2',
+    c: '3'
+    //more string property will continueing
+}
+
+// type MyObj3=Record<string,string> //more property deya zay
+type MyObj3 = Record<'a' | 'b' | 'c', string> //more secure and modern
+const myObj3: MyObj3 = {
+    a: '1',
+    b: '2',
+    c: '3'
+    //property type MyObj3 er a,b,c porzonto hobe
+}
+
